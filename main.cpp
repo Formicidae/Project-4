00d05e49085d11e8c1b07896809ff1d45bac176c
Formicidae <eddie9ner@gmail.com>
2016-11-08 12:26:59 -0600

    Created project

---
+++ main.cpp
@@ -0,0 +1,96 @@
+//Edward Bates emb160030 section 002
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    string infileName = "poly.txt";
+    ifstream infile(infileName);
+    string outfileName = "answers.txt";
+    ofstream outfile(outfileName);
+
+    while(!infile.eof()){
+        char line[256];
+        char*linePtr = line;
+        infile.getline(line,256);
+        float Fof = atof(line + 2);
+        cout << "F : " <<   Fof << endl;
+        linePtr = strstr(line,"=") + 1;
+
+        string stringK = " 5.9x^11 + x^3 - 3x^10 + 3";
+        stringK = linePtr;
+
+//        getline()
+
+        stringK += 'N';
+        char sign = '+';
+        if(stringK[1] == '-'){
+            sign = '-';
+            stringK = stringK.substr(2);
+            cout << endl << stringK << endl;
+        }
+
+
+        char nextsign = sign;
+        while(stringK.length() > 1){
+            sign = nextsign;
+            string term1 = "";
+            float exp;
+            int i = 0;
+            while(stringK[i] != '+' && stringK[i] != '-' && stringK[i] != 'N'){
+                i++;
+            }
+            cout << stringK << endl;
+            cout << "I: " << i << endl;
+            term1 = stringK.substr(0,i);
+            nextsign = stringK[i];
+            stringK = stringK.substr(i+1);
+            cout << "\t\t\tTern ="<< term1 <<  endl;
+            float fl = atof(term1.c_str());
+            cout << "Float " << fl << endl;
+            if(!term1.find('x')){
+                //constant
+
+                //break;
+            }
+            if(term1[1] == 'x'){
+                //base is 1
+                fl = 1;
+            }
+            term1 += 'N';
+            i = 0;
+            while(term1[i] != 'x' && term1[i] != 'N'){
+                i++;
+            }
+            if(term1[i+1] != '^'){
+                //exponent 1
+                if(term1[i] == 'x'){
+                    exp = 1;
+                }
+                else{
+                    exp = 0;
+                }
+            }
+            else{
+                string xp = term1.substr(i+2);
+                cout << "xp :" << xp << endl;
+                exp = atof(xp.c_str());
+            }
+
+            cout << endl << "Sign is:" << sign << "\nBase is:" << fl << "\nExp :" << exp << endl;
+
+
+        }
+
+    }
+
+
+
+    return 0;
+}
